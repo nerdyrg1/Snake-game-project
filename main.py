5fc07bd39582db184e89536d0b54330501404f89
Rishabh Gupta <guptarishabh094@gmail.com>
2023-05-25 08:49:05 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,44 +1,52 @@
-# from turtle import Turtle, Screen
-#
-# snake_object = []
-# x_positions = []
-#
-# screen = Screen()
-# screen.bgcolor("black")
-# screen.setup(width=600,height=600)
-# screen.title("My Snake Game")
-#
-# x = 0
-# for i in range(3):
-#     object1 = Turtle(shape="square")
-#     object1.color("white")
-#     object1.goto(x=x, y=0)
-#     snake_object.append(object1)
-#     x -= 20
-#
-# screen.exitonclick()
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
-class Dog:
-    def __init__(self):
-        self.temperament = "loyal"
-
-    def bark(self):
-        print("Woof, woof!")
 
 
-class Labrador(Dog):
-    def __init__(self):
-        #super().__init__()
-        self.is_a_good_boy = True
 
-    def bark(self):
-        super().bark()
-        print("Greetings, good sir. How do you do?")
-
-Labrador().bark()
-
-
-from turtle import Turtle
-
-class Food(Turtle):
+screen.exitonclick()
